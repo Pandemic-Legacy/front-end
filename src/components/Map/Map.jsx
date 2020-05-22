05a32e3959b0b3ff6b650dbd7bbd24619755b11e
Chelsea Spangler <56610933+CNSpangler@users.noreply.github.com>
2020-05-22 11:12:55 -0700

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,86 +10,24 @@ const WorldMap = () => {
   const [property, setProperty] = useState('residentialChange');
   const [rotateX, setRotateX] = useState(0);
   const [rotateY, setRotateY] = useState(0);
-<<<<<<< HEAD
-  const [deltaX, setDeltaX] = useState(null);
-  const [deltaY, setDeltaY] = useState(null);
-  // const [rotate, setRotate] = useState([0, 0, 0]);
 
   const geoJson = useWorldMobilityData('2020-05-01T00:00:00.000+00:00');
 
-  // code for mouse coordinates modified from http://bl.ocks.org/1392560
-  // let mouseStart, rotationStart;
-  // function mousedown() {
-  //   // mouseStart = [event.pageX, event.pageY];
-  //   mouseStart = mouse(svgRef);
-  //   rotationStart = rotate;
-  //   // event.preventDefault();
-  // }
-
-  // function mousemove() {
-  //   if(mouseStart) {
-  //     // const mouseEnd = [event.pageX, event.pageY];
-  //     const mouseEnd = mouse(svgRef);
-  // const rotationEnd = [rotationStart[0] + (mouseEnd[0] - mouseStart[0]) / 6, rotationStart[1] + (mouseStart[1] - mouseEnd[1]) / 6];
-  // rotationEnd[1] = rotationEnd[1] > 30  ? 30  :
-  //   rotationEnd[1] < -30 ? -30 :
-  //     rotationEnd[1];
-  // setRotate(rotationEnd);
-  //   }
-  // }
-  // function mouseup() {
-  //   if(mouseStart) {
-  //     mousemove();
-  //     mouseStart = null;
-  //   }
-  // }
-
-  // select(window)
-  //   .on('mousemove', mousemove)
-  //   .on('mouseup', mouseup);
-
-  const dragHandler = drag()
-    .on('start', function() {
-      // let current = select(svgRef.current);
-      setDeltaX(rotateX - event.x);
-      setDeltaY(rotateY - event.y);
-    })
-    .on('drag', function() {
-      setRotateX((event.x + deltaX) / 6);
-      setRotateY((event.y + deltaY) / -6);
-      console.log('deltaX', deltaX);
-      console.log('deltaY', deltaY);
-    })
-    .on('end', function() {
-      setDeltaX(rotateX);
-      setDeltaY(rotateY);
-    });
-=======
-  
   const svgRef = useRef();
   const wrapperRef = useRef();
 
   const dimensions = useResizeObserver(wrapperRef);
-  const geoJson = useWorldMobilityData('2020-05-01T00:00:00.000+00:00');
->>>>>>> d71a4cb7409a1d950312d600fbc9935b3ff6493d
-
+  
   useEffect(() => {
     if(!geoJson.features) return;
 
-<<<<<<< HEAD
-    const minProp = -100;
-    const maxProp = 100;
-    const colorScale = scaleLinear().domain([minProp, 0, maxProp]).range(['blue', 'rgb(243, 240, 225)', 'green']);
-=======
     const svg = select(svgRef.current);
->>>>>>> d71a4cb7409a1d950312d600fbc9935b3ff6493d
 
     const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect();
     
     const colorScale = scaleLinear()
       .domain([-100, 0, 100])
       .range(['blue', 'rgb(243, 240, 225)', 'green']);
-
 
     const projection = geoOrthographic()
       .fitSize([width * 0.9, height * 0.9], geoJson)
@@ -98,23 +36,6 @@ const WorldMap = () => {
       .translate([width / 2, height / 2])
       .precision(200);
 
-    // const projection = geoMercator()
-    //   .fitSize([width, height], selectedCountry || geoJson)
-    //   .rotate([rotateX, rotateY, 0])
-    //   .precision(100);
-
-<<<<<<< HEAD
-    // const graticule = geoGraticule();
-
-    const pathGenerator = geoPath().projection(projection);
-
-    // svg
-    //   .selectAll('path.graticule')
-    //   .data([graticule()])
-    //   .join('path')
-    //   .attr('class', 'graticule')
-    //   .attr('d', line => pathGenerator(line));
-=======
     const pathGenerator = geoPath().projection(projection);
 
     const defs = svg.append('defs');
@@ -152,19 +73,7 @@ const WorldMap = () => {
         setRotateY(rotate[1] - event.dy * k);
       })
     );
->>>>>>> d71a4cb7409a1d950312d600fbc9935b3ff6493d
 
-    svg
-      .selectAll('circle')
-      .data(['spot'])
-      .enter()
-      .append('circle')
-      .data([1])
-      .attr('cx', width / 2)
-      .attr('cy', height / 2)
-      .attr('r', projection.scale())
-      .style('fill', 'turquoise');
-  
     svg
       .selectAll('.country')
       .data(geoJson.features)
@@ -180,16 +89,8 @@ const WorldMap = () => {
         : 'rgba(150, 150, 150, 0.3)'
       )
       .attr('d', country => pathGenerator(country));
-<<<<<<< HEAD
-      
-    dragHandler(svg);
-      
-  }, [geoJson, dimensions, property, selectedCountry, rotateX, rotateY]);
-=======
-    
       
   }, [geoJson, dimensions, property, selectedCountry, rotateY, rotateX]);
->>>>>>> d71a4cb7409a1d950312d600fbc9935b3ff6493d
 
   return (
     <div ref={wrapperRef} className={style.Map} >
