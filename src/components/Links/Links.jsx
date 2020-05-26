4cc98e1d5f35049e7d545ea837c1877ce27b2312
Nathan Martel <nathanmartel@gmail.com>
2020-05-25 21:13:57 -0700

    Rendering dropdown options, but array needs work

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import { Grid, Link } from '@material-ui/core';
 
 import { useStyles } from './Links.styles';
-import { fetchCountryNameCovidData } from '../../services/covid';
-import { setCovidChartData, setSelectedCountryCode } from '../../actions/actions';
+import { setSelectedCountryCode } from '../../actions/actions';
 import { getGlobalMapMobilityByDate, getSelectedCountryCode } from '../../selectors/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -15,8 +14,7 @@ export const Links = () => {
   const selectedCountryCode = useSelector(getSelectedCountryCode);
   const globalMapMobilityData = useSelector(getGlobalMapMobilityByDate);
   
-  console.log('global data is', globalMapMobilityData);
-  const selectOptions = globalMapMobilityData.features.map((item, i) => (<option key={i} value={item.mobilityData.countryCode}>{item.mobilityData.countryName}</option>));
+  const selectOptions = () => globalMapMobilityData.features.map((item, i) => <option key={i} value={item.mobilityData.countryCode}>{item.mobilityData.countryName}</option>);
 
   // const handleChange = ({ target }) => {
   //   setSearchedCountry(target.value);
@@ -33,9 +31,18 @@ export const Links = () => {
       <Grid item xs={12} className={classes.links}>
         {/* <form name="country" onSubmit={handleSubmit}> */}
         {/* <input type="text" value={searchedCountry} onChange={handleChange} className={classes.search} placeholder="Search for country"/> */}
+        { console.log({ globalMapMobilityData })}
+        { globalMapMobilityData.features &&
         <select value={selectedCountryCode} onChange={({ target }) => dispatch(setSelectedCountryCode(target.value))}>
-          {selectOptions}
+          {/* Or do this */}
+          {/* {globalMapMobilityData.features.map((item, i) => (
+            <option key={i} value={item.mobilityData.countryCode}>
+              {item.mobilityData.countryName}
+            </option>
+          ))} */}
+          {selectOptions()}
         </select>
+        }
 
         {/* </form>       */}
         <Link className={classes.link} href="/highscore">High Score</Link>
