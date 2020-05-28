00760c3855759b8e2df5509c4cde0d5250b7b00a
Nathan Martel <nathanmartel@gmail.com>
2020-05-28 14:33:08 -0700

    Redux working

--- src/components/ComparePage/ComparePage.jsx
+++ src/components/ComparePage/ComparePage.jsx
@@ -2,28 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Grid, Typography, FormControl, Input, InputLabel, Select, MenuItem, CircularProgress } from '@material-ui/core';
 import { useStyles } from './ComparePage.styles';
 // import Map from '../Map/Map';
-import { getGlobalMapMobilityByDate, getSelectedCountryCode, getMobilitySubregionNames, getSelectedSubregion, getCovidSubData, getMobilitySubData, getSelectedCountryName } from '../../selectors/selectors';
+import { getGlobalMapMobilityByDate, getSelectedCountryCode, getMobilitySubregionNames, getSelectedSubregion, getCovidSubData, getMobilitySubData, getSelectedCountryName, getMobilityCompareCountryCode, getMobilityCompareCountryName } from '../../selectors/selectors';
 import { useParams } from 'react-router-dom';
-import { getCovidChartData } from '../../selectors/selectors';
+// import { getCovidChartData } from '../../selectors/selectors';
 import { useSelector, useDispatch } from 'react-redux';
 import MiniChartsContainer from '../MiniChart/MiniChartsContainer';
-import { setSelectedSubregion, setMobilitySubregionNames, setCovidSubData, setMobilitySubData, resetCovidSubData, setSelectedCountryCode, setSelectedCountry, setSelectedCountryName } from '../../actions/actions';
+import { setSelectedSubregion, setMobilitySubregionNames, setCovidSubData, setMobilitySubData, resetCovidSubData, setSelectedCountryCode, setSelectedCountry, setSelectedCountryName, setMobilityCompareCountry } from '../../actions/actions';
 import { useMobilityDataByCountryCode } from '../../hooks/mobilityHooks';
 
 export const ComparePage = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const [selectedCompareCountryCode, setSelectedCompareCountryCode] = useState('');
-  const [mobilityCompareData, setMobilityCompareData] = useState({});
+  // const [selectedCompareCountryCode, setSelectedCompareCountryCode] = useState('');
+  // const [mobilityCompareData, setMobilityCompareData] = useState({});
   const { countryCode: countryCodeParam } = useParams();
   const countryCode = useSelector(getSelectedCountryCode);
   const countryName = useSelector(getSelectedCountryName);
   const subregion = useSelector(getSelectedSubregion);
   const subRegionNames = useSelector(getMobilitySubregionNames);
-  const chartDataSet = useSelector(getCovidChartData);
+  // const chartDataSet = useSelector(getCovidChartData);
   const stackGraphSubData = useSelector(getCovidSubData);
   const selectedCountryCode = useSelector(getSelectedCountryCode);
   const selectedCountryName = useSelector(getSelectedCountryName);
+  const compareCountryCode = useSelector(getMobilityCompareCountryCode);
+  const compareCountryName = useSelector(getMobilityCompareCountryName);
   const globalMapMobilityData = useSelector(getGlobalMapMobilityByDate);
   
 
@@ -33,11 +35,11 @@ export const ComparePage = () => {
     if(!countryCode.length) dispatch(setSelectedCountryCode(countryCodeParam));
   }, []);
 
-  useEffect(() => {
-    if(!selectedCompareCountryCode) return;
-    const compareData = useMobilityDataByCountryCode(selectedCompareCountryCode);
-    setMobilityCompareData(compareData);
-  }, [selectedCompareCountryCode]);
+  // useEffect(() => {
+  //   if(!selectedCompareCountryCode) return;
+  //   const compareData = useMobilityDataByCountryCode(selectedCompareCountryCode);
+  //   setMobilityCompareData(compareData);
+  // }, [selectedCompareCountryCode]);
 
   
   // useEffect(() => {
@@ -59,6 +61,8 @@ export const ComparePage = () => {
   //   ?.sort()
   //   .map((item) => (<MenuItem  key={item} value={item}>{item}</MenuItem>));
 
+
+  // Build selects for compare country dropdown
   const selectOptions = globalMapMobilityData?.features
     ?.filter(item => item.mobilityData.countryName != null)
     .sort((a, b) => (a.mobilityData.countryName > b.mobilityData.countryName) ? 1 : -1)
@@ -74,21 +78,17 @@ export const ComparePage = () => {
 
   return (
     <Grid container justify="center" className={classes.root}>
-      <Grid item xs={12} md={10}>
-        <Typography variant="h3" color="primary" className={classes.title}>{countryName}</Typography>
-      </Grid>
 
-      {/* Second country select field */}
+      {/* Compare country select field */}
       <Grid item xs={12} sm={3}>
         <FormControl variant="outlined" size="small" fullWidth className={classes.formControl}>
           {/* <InputLabel id="country-select-label">Choose a Country</InputLabel> */}
-          
           <Select
-            labelId="country-select-label"
-            id="country-select"
+            labelId="compare-select-label"
+            id="compare-select"
             value={JSON.stringify({
-              countryCode: selectedCountryCode,
-              countryName: selectedCountryName
+              countryCode: compareCountryCode,
+              countryName: compareCountryName
             })}
             onChange={({ target }) => {
               const { countryCode, countryName } = JSON.parse(target.value);
@@ -96,12 +96,14 @@ export const ComparePage = () => {
                 countryCode,
                 countryName
               };
-              setSelectedCompareCountryCode(toDispatch.countryCode);
-              // dispatch(setSelectedSubregion(''));
+              // If using hooks...
+              // setMobilityCompareCountryCode(toDispatch.countryCode);
+              dispatch(setMobilityCompareCountry(toDispatch));
+              // dispatch(setSelectedCompareSubregion(''));
               // if(location.pathname !== '/')history.replace(`/compare/${countryCode}`);
             }}
           >
-            { (location.pathname === '/') &&
+            { (location.pathname === '/compare/') &&
               <MenuItem value={JSON.stringify({
                 countryCode: '',
                 countryName: 'Worldwide'
@@ -131,6 +133,10 @@ export const ComparePage = () => {
           </FormControl>}
       </Grid> */}
       
+      <Grid item xs={12} md={10}>
+        <Typography variant="h3" color="primary" className={classes.title}>{countryName}</Typography>
+      </Grid>
+
       <Grid item xs={12} md={10} className={classes.graph}>
         <MiniChartsContainer />
       </Grid>
